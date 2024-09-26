33f4e9c1d90e956f4eb9a40f0a2a9c91ac28a08d
brian.orwe <brian.orwe@gmail.com>
2024-09-26 18:19:00 +0300

    Initial setup

---
+++ wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+config.color_scheme = require("bg")
+config.hide_tab_bar_if_only_one_tab = true
+config.font_size=14.0
+config.window_frame = {
+        font_size = 11.0
+}
+
+return config
